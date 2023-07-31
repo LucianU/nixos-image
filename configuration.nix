59a9fb94ea091915cd94a7a250923c9ba0659388
Lucian Ursu <lucian.ursu@gmail.com>
2023-07-31 15:43:03 +0300

    Change a few attributes
    Add a stateVersion and enable documentation.

--- configuration.nix
+++ configuration.nix
@@ -3,10 +3,6 @@
   imports = [
   ];
 
-  # This tells Nix to send the build to the remote builder when running on Mac
-  # Without this, the build fails on Mac
-  nixpkgs.system = "x86_64-linux";
-
   boot.cleanTmpDir = true;
 
   networking.firewall.allowPing = true;
@@ -22,6 +18,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPI+dFnzXZ0xACwp1x9hMH0FFx4+WLj7ZiXW+j2z58sc lucian@Lucians-MacBook-Pro-2.local"
   ];
 
-  documentation.nixos.enable = false;
-
+  nixpkgs.system = "x86_64-linux";
+  system.stateVersion = "23.05";
+  documentation.nixos.enable = true;
 }
